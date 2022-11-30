a20afa542e1f926abc9a936ba2399796c48e86a0
Aayush Shah <shahaayush21@gmail.com>
2022-12-01 00:40:37 +0530

    css

--- login.php
+++ login.php
@@ -7,9 +7,9 @@
 
 <?php include_once "header.php"; ?>
 <body>
-  <div class="wrapper">
+  <div class="wrapper1">
     <section class="form login">
-      <header>Realtime Chat App</header>
+      <header>Nebula Chat</header>
       <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
         <div class="error-text"></div>
         <div class="field input">
